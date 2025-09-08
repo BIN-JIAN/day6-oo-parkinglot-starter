2d4494dc669b224f9ffdf1cb896b776db0d9b3ba
BIN <BIN.JIAN@OOCL.COM>
2025-09-08 21:46:41 +0800

    feat: add function for story3 case 4

--- src/main/java/com/afs/parkinglot/ParkingBoy.java
+++ src/main/java/com/afs/parkinglot/ParkingBoy.java
@@ -2,16 +2,31 @@ package com.afs.parkinglot;
 
 public class ParkingBoy {
     private ParkingLot parkingLot;
+    private String lastErrorMessage;
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
     public Ticket park(Car car) {
-        return parkingLot.park(car);
+        try {
+            return parkingLot.park(car);
+        } catch (NoAvailablePositionException e) {
+            lastErrorMessage = e.getMessage();
+            return null;
+        }
     }
 
     public Car fetch(Ticket ticket) {
-        return parkingLot.fetch(ticket);
+        try {
+            return parkingLot.fetch(ticket);
+        } catch (UnrecognizedTicketException e) {
+            lastErrorMessage = e.getMessage();
+            return null;
+        }
+    }
+
+    public String getLastErrorMessage() {
+        return lastErrorMessage;
     }
 }
