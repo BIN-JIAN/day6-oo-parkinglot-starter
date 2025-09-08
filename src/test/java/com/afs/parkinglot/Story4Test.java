b1484d6088d287a69bc5dc540a8739ad652584b4
BIN <BIN.JIAN@OOCL.COM>
2025-09-08 22:41:58 +0800

    test: add test for story 4 case 4

--- src/test/java/com/afs/parkinglot/Story4Test.java
+++ src/test/java/com/afs/parkinglot/Story4Test.java
@@ -52,4 +52,50 @@ public class Story4Test {
         assertEquals(secondCar, fetchedCar);
 
     }
+
+    @Test
+    void should_return_right_cars_when_standard_boy_fetch_twice_with_two_tickets() {
+        // Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLots);
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        Ticket firstTicket = parkingBoy.park(firstCar);
+        Ticket secondTicket = parkingBoy.park(secondCar);
+
+        // When
+        Car fetchedFirstCar = parkingBoy.fetch(firstTicket);
+        Car fetchedSecondCar = parkingBoy.fetch(secondTicket);
+
+        // Then
+        assertEquals(firstCar, fetchedFirstCar);
+        assertEquals(secondCar, fetchedSecondCar);
+    }
+
+    @Test
+    void should_return_nothing_with_error_message_when_standard_boy_fetch_with_unrecognized_ticket() {
+        // Given - 一个标准停车小弟，管理两个停车场，和一个未识别的停车票
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLots);
+        Ticket unrecognizedTicket = new Ticket(); // 创建一个未使用过的票，即未识别票
+
+        // When - 取车
+        Car car = parkingBoy.fetch(unrecognizedTicket);
+
+        // Then - 返回空，并显示错误信息
+        assertNull(car);
+        assertEquals("Unrecognized parking ticket.", parkingBoy.getLastErrorMessage());
+    }
 }
