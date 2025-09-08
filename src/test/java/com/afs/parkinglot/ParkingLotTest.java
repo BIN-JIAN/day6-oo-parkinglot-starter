de1198b33dd01e816f924532d7f744710dbdad61
BIN <BIN.JIAN@OOCL.COM>
2025-09-08 21:50:16 +0800

    test: add test for story 3 case 5

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -154,4 +154,19 @@ public class ParkingLotTest {
         assertNull(fetchedCar);
         assertEquals("Unrecognized parking ticket.", parkingBoy.getLastErrorMessage());
     }
+
+    @Test
+    void should_given_parking_lot_parking_boy_and_used_ticket_when_fetch_then_return_nothing_with_error_message() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car parkedCar = new Car();
+        Ticket ticket = parkingLot.park(parkedCar);
+        parkingLot.fetch(ticket);
+        // When
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // Then
+        assertNull(fetchedCar);
+        assertEquals("Unrecognized parking ticket.", parkingBoy.getLastErrorMessage());
+    }
 }
